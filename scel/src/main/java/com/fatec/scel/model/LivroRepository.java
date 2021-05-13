6cc600a1871d19c53ab88db94139eaabb6a76b71
profe <profe@DESKTOP-EPGIS66>
2021-05-13 16:10:09 -0300

    Squashed commit of the following:
    commit 03f1c21e60cea50abc5e97491f2fb3ccc26fff46
    Author: profe <profe@DESKTOP-EPGIS66>
    Date:   Thu May 13 16:08:41 2021 -0300
    
    	REQ02ConsultarLivro - validação do comportamento

--- scel/src/main/java/com/fatec/scel/model/LivroRepository.java
+++ scel/src/main/java/com/fatec/scel/model/LivroRepository.java
@@ -1,10 +1,12 @@
 package com.fatec.scel.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends CrudRepository <Livro, Long>{
+	public Livro findByIsbn(@Param("isbn") String isbn);
 }
 
 
