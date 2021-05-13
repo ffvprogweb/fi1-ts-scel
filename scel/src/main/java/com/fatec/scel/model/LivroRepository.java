cf179fd30bdd7d287c49e9224a78a3f39e3a44d9
profe <profe@DESKTOP-EPGIS66>
2021-05-13 15:39:32 -0300

    REQ01CadastrarLivro - validacao do comportamento

---
+++ scel/src/main/java/com/fatec/scel/model/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.fatec.scel.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends CrudRepository <Livro, Long>{
+}
+
+
