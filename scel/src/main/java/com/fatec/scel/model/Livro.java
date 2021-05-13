03f1c21e60cea50abc5e97491f2fb3ccc26fff46
profe <profe@DESKTOP-EPGIS66>
2021-05-13 16:08:41 -0300

    REQ02ConsultarLivro - validação do comportamento

--- scel/src/main/java/com/fatec/scel/model/Livro.java
+++ scel/src/main/java/com/fatec/scel/model/Livro.java
@@ -54,5 +54,43 @@ public class Livro {
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (autor == null) {
+			if (other.autor != null)
+				return false;
+		} else if (!autor.equals(other.autor))
+			return false;
+		if (id != other.id)
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
 	
 }
