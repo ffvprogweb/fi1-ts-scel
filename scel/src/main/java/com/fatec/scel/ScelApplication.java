97a313f15da304f3ecb5dfb52e515056f102faed
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-06-10 13:54:38 -0300

    REQ04 altualização de livro

--- scel/src/main/java/com/fatec/scel/ScelApplication.java
+++ scel/src/main/java/com/fatec/scel/ScelApplication.java
@@ -2,7 +2,7 @@ package com.fatec.scel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// REQ04 alterar livro
 @SpringBootApplication
 public class ScelApplication {
 
